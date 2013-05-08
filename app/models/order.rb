7f1789d4ee810838d7053d0d5b6643efff963d50
Nisha Sehn <nishasehn@gmail.com>
2013-05-08 15:42:05 -0700

    Form includes sandwich options

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,14 +1,14 @@
 class Order < ActiveRecord::Base
   attr_accessible :name
   #validates_column :sandwich
-  #validates_inclusion_of :sandwich, :in => ["meat", "vegetarian", "vegan"]
+  validates_inclusion_of :sandwich, :in => ["meat", "vegetarian", "vegan"]
 
 
   # def sandwich
-  #   read_attribute(:sandwhich).to_sym
+  #   read_attribute(:sandwich).to_sym
   # end
   # def sandwhich= (value)
-  #   write_attribute(:sandwhich, value.to_s)
+  #   write_attribute(:sandwich, value.to_s)
   # end
 
 end
