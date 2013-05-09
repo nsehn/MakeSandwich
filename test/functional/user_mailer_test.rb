22cead0d0c239583a70975f8a88aa8deb0c067c3
Nisha Sehn <nishasehn@gmail.com>
2013-05-09 10:47:33 -0700

    Beginning of test to check if email delivered with correct contents

--- test/functional/user_mailer_test.rb
+++ test/functional/user_mailer_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def test_sandwich_order
+		@order = order
+
+	#This is to test if the email was queued after it was sent
+	email = UserMailer.create_sandwich_order('nishasehn@gmail.com').deliver
+	assert !ActionMailer::Base.deliveries.empty?
+
+	#Test to see if the email contains the text
+	assert_equal [order.email], email.to
+	assert_equal "Sandwich order", email.subject
+	assert_match(/<h1>Sandwich order by/, email.encoded)
+	assert_match(/Sandwich order by/, email.encoded)
+
+	end
 end
