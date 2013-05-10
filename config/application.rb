93f4db7766c687b1280a753784284dbabd01eaa3
Nisha Sehn <nishasehn@gmail.com>
2013-05-10 07:33:20 -0700

    Application router will be invoked any time there is an exception. Beginning to set up error pages

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module MakeSandwich
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #Router will be invoked everytime there is an exception
+    config.exceptions_app = self.routes
   end
 end
